e0b95fb8c587516ace0156799da67b0fa1684a73
Nicky <ne@cegeka.com>
2019-02-09 06:24:47 +0000

    fix tests

--- donkeycar/parts/sensehatled.py
+++ donkeycar/parts/sensehatled.py
@@ -6,19 +6,19 @@ class SenseHatLed:
         self.rgb = (0,0,0)
         print("init")
 
-    def set_rgb(self, r, g, b):
+    def set_rgb(self,x, y, r, g, b):
         r = r if not self.invert else 100 - r
         g = g if not self.invert else 100 - g
         b = b if not self.invert else 100 - b
         self.rgb = (r, g, b)
 
-        self.sense.set_pixel(0, 0, self.rgb)
+        self.sense.set_pixel(x, y, self.rgb)
 
 
 if __name__ == "__main__":
     import time
     senseHatLed = SenseHatLed()
-    senseHatLed.set_rgb(255, 0, 0)
+    senseHatLed.set_rgb(1,1, 255, 0, 0)
     print("set color")
     senseHatLed.set_rgb(0, 0, 255, 0, 0)
     senseHatLed.set_rgb(0, 7, 0, 255, 0)
@@ -31,12 +31,12 @@ if __name__ == "__main__":
 
     iter = 0
     while iter < 100:
-        p.set_rgb(iter, 100 - iter, 0)
+        senseHatLed.set_rgb(2,2,iter, 100 - iter, 0)
         time.sleep(delay)
         iter += 1
 
     iter = 0
     while iter < 100:
-        p.set_rgb(100 - iter, 0, iter)
+        senseHatLed.set_rgb(2,2,100 - iter, 0, iter)
         time.sleep(delay)
         iter += 1
